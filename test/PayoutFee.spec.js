fea436400019565af032d307b178eb9a7aebae1f
chanchowancho <victor.jones@gmail.com>
2017-03-19 14:10:44 +1300

    Initial Checkin PayoutFee
    83% Coverage
    Niggling sinon issue

---
+++ test/PayoutFee.spec.js
@@ -0,0 +1,73 @@
+'use strict'
+
+const expect = require('expect')
+const rewire = require('rewire')
+const config = require('config')
+const sinon = require('sinon')
+
+let PayoutFee = rewire('../src/lib/PayoutFee')
+const incomingSettings = config.get('INCOMING')
+
+
+beforeEach(() => {
+  PayoutFee = rewire('../src/lib/PayoutFee')
+})
+
+describe('[PayoutFee]', () => {
+  it('should fail on params', (done) => {
+      const callback = (success, data) => {
+        expect(success).toBe(false)
+        expect(data.message).toBeA('string')
+        done()
+      }
+      PayoutFee.run({}, callback)
+  })
+  it('should call send() when params present', (done) => {
+      const callback = (success, data) => {
+      }
+
+      PayoutFee.send = () => {
+        expect(true).toBe(true)
+        done()
+      }
+      PayoutFee.run({settings:"bloop",navClient:"wow"}, callback)
+  })
+  it('should fail when navBalance is less than poolAmount', (done) => {
+      const callback = (success, data) => {
+        expect(success).toBe(false)
+        expect(data.message).toBeA('string')
+        // sinon.assert.calledOnce(mockLogger.writeLog)
+        done()
+      }
+      const mockClient = {
+        getBalance: () => { return Promise.resolve({ navBalance: 2 }) },
+      }
+      const mockSettings = {
+        navPoolAmount: 10
+      }
+      PayoutFee.runtime = {
+        callback,
+        settings: mockSettings,
+        navClient: mockClient,
+      }
+      // const mockLogger = {
+      //   writeLog: sinon.spy(),
+      // }
+      // PayoutFee.__set__('Logger', mockLogger)
+      PayoutFee.send()
+  })
+  it('should call the callback when payoutfee.sent is false',(done) =>{
+    const callback = (success, data) => {
+        expect(success).toBe(true)
+        expect(data.message).toBeA('string')
+        done()
+    }
+
+    PayoutFee.runtime = {
+        callback
+    }
+    PayoutFee.sent(true,{message:'testData'})
+  })
+
+})
+
