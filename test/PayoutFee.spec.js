1dc79c61ac6279d258be9159aba8afb8c52ce655
chanchowancho <victor.jones@gmail.com>
2017-03-20 22:05:44 +1300

    Minor changes
    -More tests

--- test/PayoutFee.spec.js
+++ test/PayoutFee.spec.js
@@ -5,6 +5,7 @@ const rewire = require('rewire')
 const config = require('config')
 const sinon = require('sinon')
 
+
 let PayoutFee = rewire('../src/lib/PayoutFee')
 const incomingSettings = config.get('INCOMING')
 
@@ -68,5 +69,73 @@ describe('[PayoutFee]', () => {
     }
     PayoutFee.sent(true,{message:'testData'})
   })
+  it('should fail when navPayout is less than the minimum specified amount', (done) => {
+        const callback = (success, data) => {
+          expect(success).toBe(false)
+          expect(data.message).toBeA('string')
+          done()
+        }
+        const mockClient = {
+          getBalance: () => { return Promise.resolve(12) },
+        }
+        const mockSettings = {
+          navPoolAmount: 10,
+          txFeePayoutMin: 200
+        }
+        PayoutFee.privateSettings = {
+          txFee: 0.01
+        }
+        PayoutFee.runtime = {
+          callback,
+          settings: mockSettings,
+          navClient: mockClient,
+        }
+        PayoutFee.send()
+    })
+    it('sent should report back failure when payment did not succeed', (done) => {
+        const callback = (success, data) => {
+           expect(success).toBe(false)
+           expect(data.message).toBe('failed to send fee payout')
+           done()
+        }
+        const mockClient = {
+          name: 'jeeves',
+          getBalance: () => { return Promise.resolve(120) },
+        }
+        const mockSettings = {
+          navPoolAmount: 10,
+          txFeePayoutMin: 1,
+          anonTxFeeAddress: 'abc123',
+        }
+        PayoutFee.runtime = {
+          callback,
+          settings: mockSettings,
+          navClient: mockClient,
+        }
+        PayoutFee.sent(false)
+    })
+    it('sent should report back success when payment succeeded', (done) => {
+        const callback = (success, data) => {
+           expect(success).toBe(true)
+           done()
+        }
+        const mockClient = {
+          name: 'jeeves',
+          getBalance: () => { return Promise.resolve(120) },
+        }
+        const mockSettings = {
+          navPoolAmount: 10,
+          txFeePayoutMin: 1,
+          anonTxFeeAddress: 'abc123',
+        }
+        PayoutFee.runtime = {
+          callback,
+          settings: mockSettings,
+          navClient: mockClient,
+        }
+        PayoutFee.sent(true,{test:'data'})
+    })
+
+
 
 })
