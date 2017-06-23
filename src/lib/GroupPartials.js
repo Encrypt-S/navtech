9464a106a5b291a714ffb9c09732ea53060c73b5
Craig <craig.b.macgregor@gmail.com>
2017-06-23 12:52:26 +0200

    correct amount being pushed

--- src/lib/GroupPartials.js
+++ src/lib/GroupPartials.js
@@ -134,7 +134,7 @@ GroupPartials.groupPartials = (decrypted, transaction) => {
 
   const safeTotal = (satoshiAmount + satoshiPartialAmount) / 100000000
 
-  GroupPartials.runtime.partials[decrypted.u].amounts.push(GroupPartials.runtime.partials[decrypted.u].amount)
+  GroupPartials.runtime.partials[decrypted.u].amounts.push(transaction.amount)
   GroupPartials.runtime.partials[decrypted.u].amount = safeTotal
   GroupPartials.runtime.partials[decrypted.u].partsSum += parseInt(decrypted.p, 10)
   GroupPartials.runtime.partials[decrypted.u].transactions[transaction.txid] = {
